2d458fa1e0c53db46a9d28832a027263c052ebab
kele23 <scala233@gmail.com>
2014-06-03 12:18:46 +0200

    TowerMessage

--- src/it/univr/swe/messages/TowerMessage.java
+++ src/it/univr/swe/messages/TowerMessage.java
@@ -1,5 +1,23 @@
 package it.univr.swe.messages;
 
 public class TowerMessage implements Message {
+	
+	private int destination;
+	private boolean brake;
+	
+	public TowerMessage(int destination,boolean brake){
+		
+		this.destination = destination;
+		this.brake = brake;
+		
+	}
+
+	public int getDestination() {
+		return destination;
+	}
+
+	public boolean isBrake() {
+		return brake;
+	}
 
 }
