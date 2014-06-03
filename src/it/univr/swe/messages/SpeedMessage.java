aa044e21d712c689bc33ca0cdc56ab5f6b66e243
kele23 <scala233@gmail.com>
2014-06-03 12:03:09 +0200

    SpeedMessage

--- src/it/univr/swe/messages/SpeedMessage.java
+++ src/it/univr/swe/messages/SpeedMessage.java
@@ -2,6 +2,24 @@ package it.univr.swe.messages;
 
 public class SpeedMessage implements Message {
 	
-	ciao
+	private int source;
+	private int speed;
+	
+	public SpeedMessage(int source,int speed){
+		
+		this.source = source;
+		this.speed = speed;
+		
+	}
+
+	public int getSource() {
+		return source;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	
+	
 
 }
