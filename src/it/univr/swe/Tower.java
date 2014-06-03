9db52c3b0be502dd9d1fe83b8de0516d789994b0
Matteo Zanoncello <id556gbw@studenti.univr.it>
2014-06-03 12:19:23 +0200

    Aggiunta package Exception e prima classe eccezione.

--- src/it/univr/swe/Tower.java
+++ src/it/univr/swe/Tower.java
@@ -1,5 +1,49 @@
 package it.univr.swe;
 
-public class Tower {
+import it.univr.swe.communication.CarChannel;
+import it.univr.swe.communication.TowerChannel;
+import it.univr.swe.exception.IllegalMessageException;
+import it.univr.swe.messages.ExitMessage;
+import it.univr.swe.messages.Message;
+import it.univr.swe.messages.OkMessage;
+import it.univr.swe.messages.SpeedMessage;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tower
+{
+	private Map<Integer,Boolean> map;
+	private CarChannel[] carChannels;
+	private TowerChannel towerChannel;
+	
+	public Tower()
+	{
+		map = new HashMap<Integer, Boolean>();
+		carChannels = new CarChannel[5];
+		carChannels[0] = new CarChannel();
+		carChannels[1] = new CarChannel();
+		carChannels[2] = new CarChannel();
+		carChannels[3] = new CarChannel();
+		carChannels[4] = new CarChannel();
+		towerChannel = new TowerChannel();
+	}
+	
+	public void receive(Message msg)
+	{
+		if(msg instanceof OkMessage)
+		{
+			
+		}
+		else if(msg instanceof SpeedMessage)
+		{
+			
+		}
+		else if(msg instanceof ExitMessage)
+		{
+			
+		}
+		else
+			throw new IllegalMessageException();
+	}
 }
---
+++ src/it/univr/swe/exception/IllegalMessageException.java
@@ -0,0 +1,11 @@
+package it.univr.swe.exception;
+
+public class IllegalMessageException extends IllegalArgumentException
+{
+	private static final long serialVersionUID = 1L;
+	
+	public IllegalMessageException()
+	{
+		super();
+	}
+}
