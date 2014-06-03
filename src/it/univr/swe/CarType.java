777e54d4d9253c04b534c09979624bdd3ef316c1
kele23 <scala233@gmail.com>
2014-06-03 12:01:08 +0200

    OkMessage

--- src/it/univr/swe/CarType.java
+++ src/it/univr/swe/CarType.java
@@ -1,5 +1,8 @@
 package it.univr.swe;
 
 public enum CarType {
+	
+	MANUAL,
+	AUTOMATIC
 
 }
--- src/it/univr/swe/messages/OkMessage.java
+++ src/it/univr/swe/messages/OkMessage.java
@@ -1,5 +1,26 @@
 package it.univr.swe.messages;
 
+import it.univr.swe.CarType;
+
 public class OkMessage implements Message {
+	
+	private int source;
+	private CarType type;
+	
+	public OkMessage(int source,CarType type){
+		
+		this.source = source;
+		this.type = type;
+		
+	}
+	
+	public int getSource() {
+		return source;
+	}
+	public CarType getType() {
+		return type;
+	}
+	
+	
 
 }
