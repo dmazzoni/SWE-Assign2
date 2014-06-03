82482e6a643e3ed75c54b66ef4394ba04531b9f6
kele23 <scala233@gmail.com>
2014-06-03 12:13:56 +0200

    RegisterMessage

--- src/it/univr/swe/messages/RegisterMessage.java
+++ src/it/univr/swe/messages/RegisterMessage.java
@@ -1,5 +1,25 @@
 package it.univr.swe.messages;
 
+import it.univr.swe.communication.CarChannel;
+
 public class RegisterMessage implements Message {
+	
+	private int destination;
+	private CarChannel channel;
+	
+	public RegisterMessage(int destination,CarChannel channel){
+		
+		this.destination = destination;
+		this.channel = channel;
+		
+	}
+
+	public int getDestination() {
+		return destination;
+	}
+
+	public CarChannel getChannel() {
+		return channel;
+	}
 
 }
