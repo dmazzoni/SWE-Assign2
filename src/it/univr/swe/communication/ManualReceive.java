80ad2bee8aa394cffad1535b45313c62b9e63f62
Davide Mazzoni <davidemazzoni@yahoo.it>
2014-06-03 15:49:13 +0200

    Implementazione AutomaticReceive e ManualReceive

--- src/it/univr/swe/communication/ManualReceive.java
+++ src/it/univr/swe/communication/ManualReceive.java
@@ -5,26 +5,49 @@ import it.univr.swe.messages.*;
 
 public class ManualReceive extends ReceiveBehavior {
 	
-	public ManualReceive(Car car) {
+	public ManualReceive(ManualCar car) {
 		super(car);
 	}
 
 	@Override
 	protected void receiveJoin(JoinMessage msg) {
-		// TODO Auto-generated method stub
 		
+		if (car.getCarChannel() == null) {
+			TowerChannel ch = msg.getChannel();
+			car.setTowerChannel(ch);
+			OkMessage reply = new OkMessage(car.getId(), CarType.MANUAL);
+			car.send(reply);
+		}
 	}
 
 	@Override
 	protected void receiveTower(TowerMessage msg) {
-		// TODO Auto-generated method stub
 		
+		if (msg.getDestination() != car.getId())
+			return;
+		
+		if (msg.isBrake()) {
+			car.setDisplay("Decrease speed");
+		}
+		else {
+			car.setDisplay("Ideal");
+		}
 	}
 
 	@Override
 	protected void receiveRegister(RegisterMessage msg) {
-		// TODO Auto-generated method stub
 		
+		if (msg.getDestination() != car.getId())
+			return;
+		
+		CarChannel ch = msg.getChannel();
+		if (ch != null) {
+			car.setCarChannel(ch);
+			car.setDisplay("Car accepted");
+		}
+		else {
+			car.setDisplay("Base station is busy, please wait");
+		}
 	}
 
 }
